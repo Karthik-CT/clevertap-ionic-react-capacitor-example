530b9bb16fafbd78ef4b5958d0ca0256d7908f5f
Karthik Iyer <karthik.iyer@clevertap.com>
2022-08-10 20:16:14 +0530

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,57 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
+import { CleverTap } from "@ionic-native/clevertap";
+import { Component } from "react";
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+export class Home extends Component {
+  constructor(props: any) {
+    super(props);
+    CleverTap.setDebugLevel(3);
+    CleverTap.createNotificationChannel(
+      "testkk123",
+      "KK Notification",
+      "KK Notification Test",
+      5,
+      true
+    );
+    CleverTap.onUserLogin({
+      Name: "Test 38",
+      Identity: "test38",
+      Email: "test38@test.com",
+      Phone: "+91123456789",
+      Gender: "M",
+      "MSG-email": true,
+      "MSG-push": true,
+      "MSG-sms": true,
+      "MSG-whatsapp": true,
+    });
+  }
+  render() {
+    return (
+      <IonPage>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle>Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
-};
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Blank</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <ExploreContainer />
+        </IonContent>
+      </IonPage>
+    );
+  }
+}
 
 export default Home;
